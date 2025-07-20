0137621c1a66ad0b469c0049c1ce3daaf15c5c7f
Laurent Lanteigne <laurent.lanteigne@gmail.com>
2025-07-20 10:57:08 -0500

    removing old plugins from configs.

--- nvim/lua/plugins/copilot.lua
+++
@@ -1,8 +0,0 @@
-return {
-    "github/copilot.vim",
-    config = function()
-      -- Optionally configure copilot.vim here
-      vim.g.copilot_no_maps = true
-      vim.api.nvim_set_keymap("i", "<C-a>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
-    end,
-}
